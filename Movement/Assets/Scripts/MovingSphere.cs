3c9d8662956c8efff6bc18a4a177a25ef9b3fe14
jackie <jackie123768@gmail.com>
2023-12-24 16:30:31 -0800

    work on movement

--- Movement/Assets/Scripts/MovingSphere.cs
+++ Movement/Assets/Scripts/MovingSphere.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
 
+    void Update()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+    }
 }
